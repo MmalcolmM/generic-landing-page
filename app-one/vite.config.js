e08f509fe46096be813ef4631e2b66f1b319ced2
Malcolm Franklin <mac.mac5@yahoo.com>
2024-11-28 11:26:15 -0700

    ready for render

--- app-one/vite.config.js
+++ app-one/vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: './', // Ensures relative paths in production
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist', // Specifies the output folder for built files
+    rollupOptions: {
+      input: './index.html', // Ensures Rollup starts from the correct entry point
+    },
+  },
+});
